2207b2b4114b4fcb497d4441a763ec27e5bd2878
keepseasy <keepseasy@gmail.com>
2022-11-23 15:12:33 +0300

    очистка от последствий дебага

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 #double build to make links work properly
 texfot pdflatex --shell-escape -halt-on-error -output-directory=output ./Threads-of-Fate.tex
-#texfot pdflatex --shell-escape -halt-on-error -output-directory=output ./Threads-of-Fate.tex
-#--enable-pipes
+texfot pdflatex --shell-escape -halt-on-error -output-directory=output ./Threads-of-Fate.tex
