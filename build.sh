291c62a4a6371540c1635fd2fdcfb574eca7beca
keepseasy <35541855+keepseasy@users.noreply.github.com>
2023-01-17 17:13:58 +0300

    исправлен скрипт локальной генерации

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+mkdir -p output
+rm -f output/*
 #double build to make links work properly
 texfot pdflatex --shell-escape -halt-on-error -output-directory=output ./Threads-of-Fate.tex
 texfot pdflatex --shell-escape -halt-on-error -output-directory=output ./Threads-of-Fate.tex
