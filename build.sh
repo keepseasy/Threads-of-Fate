32fc9b00100615eb2adc8e79dbefd6e5dc26ab97
keepseasy <keepseasy@gmail.com>
2023-10-29 18:28:09 +0300

    изменена логика сборки книги

--- build.sh
+++ build.sh
@@ -3,4 +3,7 @@ mkdir -p output
 rm -f output/*
 #double build to make links work properly
 texfot pdflatex --shell-escape -halt-on-error -output-directory=output ./Threads-of-Fate.tex
+for name in `ls output/*.idx`; do
+ makeindex $name
+done
 texfot pdflatex --shell-escape -halt-on-error -output-directory=output ./Threads-of-Fate.tex
