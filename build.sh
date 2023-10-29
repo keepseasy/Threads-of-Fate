b252f2acaf1ea50b50c76d80d42f61a99565feff
keepseasy <keepseasy@gmail.com>
2023-10-29 18:56:56 +0300

    изменена логика сборки

--- build.sh
+++ build.sh
@@ -2,11 +2,5 @@
 mkdir -p output
 rm -f output/*
 #double build to make links work properly
-pdflatex --enable-pipes --shell-escape -output-directory=output ./Threads-of-Fate.tex
-cd output
-for name in `ls *.idx`; do
- makeindex $name
-done
-cd ..
-pdflatex --enable-pipes --shell-escape -output-directory=output ./Threads-of-Fate.tex
-pdflatex --enable-pipes --shell-escape -output-directory=output ./Threads-of-Fate.tex
+pdflatex --enable-pipes --shell-escape ./Threads-of-Fate.tex
+pdflatex --enable-pipes --shell-escape ./Threads-of-Fate.tex
