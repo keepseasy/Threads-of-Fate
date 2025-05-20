9c6e50a8909ef83d2bc7ff5ef72d6b31f0b1859f
keepseasy <keepseasy@gmail.com>
2025-05-20 19:50:21 +0300

    most chapters reviewed

--- scripts/leisure.py
+++ scripts/leisure.py
@@ -6,12 +6,18 @@ def genEntity(entityDict,idx,form):
  outStr=''
  for key in entityDict:
   entity=entityDict.get(key)
-  outStr+='\\subsection{'+key+'}'
-  outStr+='\\textbf{Риск:}'+try_to_get('риск', entity, key)
-  outStr+='\\newline\\textbf{СП:}'+try_to_get('СП', entity, key)
-  outStr+='\\paragraph{Описание:}'+try_to_get('описание', entity, key)
-  outStr+='\\paragraph{Эффекты:}'+try_to_get('эффекты', entity, key)
-  outStr+='\\paragraph{Проблемы:}'+try_to_get('проблемы', entity, key)
+  outStr+='\\subsection{'+key
+  if 'Отдых' in entity: outStr+=' (Отдых)'
+  outStr+='}'
+  if 'Антракт' in entity:
+    outStr+='\\textbf{Длительность: Антракт}'
+  else:
+    outStr+='\\textbf{Длительность: Интерлюдия}'
+  outStr+='\\newline\\textbf{Риск: }'+try_to_get('риск', entity, key)
+  outStr+='\\newline\\textbf{СП: }'+try_to_get('СП', entity, key)
+  outStr+='\\paragraph{Описание: }'+try_to_get('описание', entity, key)
+  outStr+='\\paragraph{Эффект: }'+try_to_get('эффект', entity, key)
+  outStr+='\\paragraph{Проблемы: }'+try_to_get('проблемы', entity, key)
  return outStr
 
 #[название]:
